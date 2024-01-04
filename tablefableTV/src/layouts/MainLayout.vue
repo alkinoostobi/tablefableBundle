fb7d6ecc50a93d47b170c279d526f9566cd0b869
Alkinoos <alkinoos@hoteplus.com>
2024-01-04 16:03:20 +0200

    changes

--- tablefableTV/src/layouts/MainLayout.vue
+++ tablefableTV/src/layouts/MainLayout.vue
@@ -1,8 +1,22 @@
 
   <template>
     <q-layout view="lHh Lpr lFf">
-      <div v-for="(initiative, index) in combat.initiatives" :key="index" :class="{ 'highlighted-box': index === combat.playerturn }">
-        {{ initiative }}
+      <div class="row">
+        <div class="banner" style="background-image: url('/svg/bannerRed.svg')">
+          <div v-for="(initiative, index) in combat.initiatives" :key="index" class="initiative">
+            {{ initiative }}
+          </div>
+        </div>
+        <div class="banner" style="background-image: url('/svg/bannerPlayer.svg')">
+          <div v-for="(initiative, index) in combat.initiatives" :key="index" class="initiative">
+            {{ initiative }}
+          </div>
+        </div>
+        <div class="banner" style="background-image: url('/svg/bannerBlack.svg')">
+          <div v-for="(initiative, index) in combat.initiatives" :key="index" class="initiative">
+            {{ initiative }}
+          </div>
+        </div>
       </div>
     </q-layout>
   </template>
@@ -21,8 +35,23 @@
     },
   });
   </script>
+
   <style>
-  .highlighted-box {
-    background-color: yellow;
+  .row {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .banner {
+    width: 293.44px;
+    height: 719.66px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin-right: 10px;
+  }
+
+  .initiative {
+    margin-bottom: 10px;
   }
   </style>
